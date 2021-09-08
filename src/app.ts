4bf414dce9e82993a71973845be6a2741010a78d
Shiv10 <shivanshsharma2012@gmail.com>
2021-09-08 23:07:54 +0530

    fix: add bodyparser

--- src/app.ts
+++ src/app.ts
@@ -3,16 +3,22 @@ import express from 'express';
 import { Request, Response } from 'express';
 import predictRouter from './routes/predictRouter';
 import reportRouter from './routes/reportRouter';
+import bodyParser from 'body-parser';
 
 const app: express.Application = express();
 const PORT = process.env.PORT;
 
-app.get('/', (req: Request, res: Response ) =>{
-    res.send("test");
-})
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.use('/predict', predictRouter);
 app.use('/report', reportRouter);
 
+app.get('/', (req: Request, res: Response ) =>{
+    res.send("test");
+});
+
 app.listen(PORT, ()=> {
-    console.log("Server is running.")
-})
+    console.log("Server is running.");
+});
