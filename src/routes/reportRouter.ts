12659ba16d40a06df3f66c605d116ae378847284
Shiv10 <shivanshsharma2012@gmail.com>
2021-09-08 23:03:11 +0530

    feat: add report router

--- src/routes/reportRouter.ts
+++ src/routes/reportRouter.ts
@@ -4,8 +4,7 @@ import { Request, Response } from 'express';
 const router = express.Router();
 
 router.post('/', (req: Request, res: Response) => {
-    // this router will be used for prediction using machine learning model
+    // this router will be used to report an incident to the authorities who will take proper action.
     res.status(200);
 });
-
 export default router;
