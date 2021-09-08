115236e971b65f3714a71ca1ccbabde2685b2bb3
Shiv10 <shivanshsharma2012@gmail.com>
2021-09-08 23:05:11 +0530

    feat: add routers

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 require('dotenv').config();
 import express from 'express';
 import { Request, Response } from 'express';
+import predictRouter from './routes/predictRouter';
+import reportRouter from './routes/reportRouter';
 
 const app: express.Application = express();
 const PORT = process.env.PORT;
@@ -8,6 +10,8 @@ const PORT = process.env.PORT;
 app.get('/', (req: Request, res: Response ) =>{
     res.send("test");
 })
+app.use('/predict', predictRouter);
+app.use('/report', reportRouter);
 
 app.listen(PORT, ()=> {
     console.log("Server is running.")
