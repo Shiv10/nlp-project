576648517f423040a5678dcb00cc853e6fa15063
Shiv10 <shivanshsharma2012@gmail.com>
2021-11-10 15:41:00 +0530

    fix: disable button on click

--- Frontend/nlp-frontend/src/Components/Form.js
+++ Frontend/nlp-frontend/src/Components/Form.js
@@ -3,8 +3,10 @@ import React, { useRef, useState } from 'react'
 function Form() {
     const textRef =  useRef();
     const [resp, setResp] = useState("");
+    const [disabled, setDisabled] = useState(false);
     const handleSubmit = async (e) => {
         e.preventDefault();
+        setDisabled(true);
         setResp("");
         const d = {
             text: textRef.current.value
@@ -16,13 +18,14 @@ function Form() {
         }; 
         let res = await fetch('http://localhost:3001/predict', requestOptions);
         res = await res.text();
-        setResp(res);        
+        setResp(res);
+        setDisabled(false);        
     }
     return (
         <div>
             <form onSubmit={handleSubmit}>
                 <input type="text" ref={textRef}/>
-                <button type="submit">Submit</button>
+                <button type="submit" disabled={disabled}>Submit</button>
             </form>
             <br/>
             <h3>{resp}</h3>
