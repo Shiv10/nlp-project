1c575b11549e0711955c7cc9753fd5a048aa38ea
Shaarang Singh <shaarangg.singh@gmail.com>
2021-11-10 19:20:07 +0530

    feat: Styled the front-end

--- Frontend/nlp-frontend/src/App.js
+++ Frontend/nlp-frontend/src/App.js
@@ -1,13 +1,17 @@
-import './App.css';
-import Form from './Components/Form';
+import "./App.css";
+import Form from "./Components/Form";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>NLP Project</h1>
-      <Form/>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>NLP Project</h1>
+			<div className="form_bg">
+				<div className="form-container">
+					<Form />
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
--- Frontend/nlp-frontend/src/Components/Form.js
+++ Frontend/nlp-frontend/src/Components/Form.js
@@ -1,36 +1,39 @@
-import React, { useRef, useState } from 'react'
+import React, { useRef, useState } from "react";
 
 function Form() {
-    const textRef =  useRef();
-    const [resp, setResp] = useState("");
-    const [disabled, setDisabled] = useState(false);
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        setDisabled(true);
-        setResp("");
-        const d = {
-            text: textRef.current.value
-        }
-        const requestOptions = {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(d)
-        }; 
-        let res = await fetch('http://localhost:3001/predict', requestOptions);
-        res = await res.text();
-        setResp(res);
-        setDisabled(false);        
-    }
-    return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <input type="text" ref={textRef}/>
-                <button type="submit" disabled={disabled}>Submit</button>
-            </form>
-            <br/>
-            <h3>{resp}</h3>
-        </div>
-    )
+	const textRef = useRef();
+	const [resp, setResp] = useState("");
+	const [disabled, setDisabled] = useState(false);
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		setDisabled(true);
+		setResp("");
+		const d = {
+			text: textRef.current.value,
+		};
+		const requestOptions = {
+			method: "POST",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify(d),
+		};
+		let res = await fetch("http://localhost:3001/predict", requestOptions);
+		res = await res.text();
+		setResp(res);
+		setDisabled(false);
+	};
+	return (
+		<div>
+			<form onSubmit={handleSubmit} className="form">
+				<textarea className="inp" ref={textRef} placeholder="Enter text" />
+				{/* <input className="inp" type="" ref={textRef} />
+				<br /> */}
+				<button className="btn" type="submit" disabled={disabled}>
+					Submit
+				</button>
+			</form>
+			<div className="res">{resp}</div>
+		</div>
+	);
 }
 
-export default Form
+export default Form;
