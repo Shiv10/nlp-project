e831679fbad77b2bb6a5c366f79d3a56b8924ef3
Shiv10 <shivanshsharma2012@gmail.com>
2022-01-15 19:16:06 +0530

    fix: run in server

--- Backend/src/routes/predictRouter.ts
+++ Backend/src/routes/predictRouter.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { Request, Response } from "express";
 import { PythonShell } from "python-shell";
+import { exec } from "child_process";
 
 const router = express.Router();
 
@@ -15,27 +16,52 @@ router.post("/", (req: Request, res: Response) => {
 		args: arr,
 	};
 
-	PythonShell.run("runModel.py", options, (err: any, results: any) => {
-		if (err) {
-			res.status(500);
-			return res.send("Internal server error");
+	exec(`python3 -u src/routes/modelUtility/runModel.py ${req.body.text}`, (error, stdout, stderr) => {
+		if (error) {
+			console.log(`error: ${error.message}`);
+			return res.send("Internal server error");;
 		}
-		if (results[0] === "0") {
-			return res.send("Abusive Comment");
+		if (stderr) {
+			console.log(`stderr: ${stderr}`);
+			return res.send("Internal server error");;
 		}
-
 		res.status(200);
-		if (results[0] === "1") {
-			return res.send("Hateful Comment");
-		}
+		console.log(stdout);
+		// if (results[0] === "0") {
+		// 	return res.send("Abusive Comment");
+		// }
+		// if (results[0] === "1") {
+		// 	return res.send("Hateful Comment");
+		// }
 
-		if (results[0] === "2") {
-			return res.send("Normal Comment");
-		}
-		if (results[0] === "3") {
-			return res.send("Spam Comment");
-		}
+		// if (results[0] === "2") {
+		// 	return res.send("Normal Comment");
+		// }
+		// if (results[0] === "3") {
+		// 	return res.send("Spam Comment");
+		// }
 	});
+
+	// PythonShell.run("runModel.py", options, (err: any, results: any) => {
+	// 	if (err) {
+	// 		res.status(500);
+	// 		return res.send("Internal server error");
+	// 	}
+	// 	res.status(200);
+	// 	if (results[0] === "0") {
+	// 		return res.send("Abusive Comment");
+	// 	}
+	// 	if (results[0] === "1") {
+	// 		return res.send("Hateful Comment");
+	// 	}
+
+	// 	if (results[0] === "2") {
+	// 		return res.send("Normal Comment");
+	// 	}
+	// 	if (results[0] === "3") {
+	// 		return res.send("Spam Comment");
+	// 	}
+	// });
 });
 
 export default router;
